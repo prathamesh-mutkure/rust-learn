88ec62cda51d77ab387eb5027b9c3b9a0c939cd2
Prathamesh Mutkure <pmutkure009@gmail.com>
2024-10-02 19:09:51 +0530

    Basics: Deck of Cards

---
+++ Projects/cards-deck/src/main.rs
@@ -0,0 +1,45 @@
+use rand::{seq::SliceRandom, thread_rng};
+
+#[derive(Debug)]
+struct Deck {
+    cards: Vec<String>,
+}
+
+impl Deck {
+    fn new() -> Self {
+        let suits = ["Hearts", "Diamonds", "Spades"];
+        let values = ["Ace", "Two", "Three"];
+
+        let mut cards: Vec<String> = vec![];
+
+        for suit in suits {
+            for value in values {
+                let card = format!("{} of {}", value, suit);
+                cards.push(card);
+            }
+        }
+
+        Deck { cards }
+    }
+
+    fn shuffle(&mut self) {
+        let mut rng = thread_rng();
+        self.cards.shuffle(&mut rng);
+    }
+
+    fn deal(&mut self, num_cards: usize) -> Vec<String> {
+        self.cards.split_off(self.cards.len() - num_cards)
+    }
+}
+
+fn main() {
+    let mut deck = Deck::new();
+
+    deck.shuffle();
+
+    // TODO: Handle errors for overflows
+    let cards = deck.deal(3);
+
+    println!("Here are your cards - {:#?}", cards);
+    println!("Here is deck - {:#?}", deck);
+}
