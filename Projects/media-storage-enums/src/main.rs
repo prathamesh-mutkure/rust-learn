cdb73fb9017347f885742cb1b65edd27b3941096
Prathamesh Mutkure <pmutkure009@gmail.com>
2024-10-03 12:08:59 +0530

    Modules

--- Projects/media-storage-enums/src/main.rs
+++ Projects/media-storage-enums/src/main.rs
@@ -1,72 +1,7 @@
-#[derive(Debug)]
-enum Media {
-    Book { title: String, author: String },
-    Movie { title: String, director: String },
-    Audiobook { title: String },
-    Podcast(u32),
-    Placeholder,
-}
+mod content;
 
-impl Media {
-    fn description(&self) -> String {
-        match self {
-            Media::Book { title, author } => {
-                format!("Book {} by {}", title, author)
-            }
-            Media::Movie { title, director } => {
-                format!("Movie {} by {}", title, director)
-            }
-            Media::Audiobook { title } => {
-                format!("Audiobook {}", title)
-            }
-            Media::Podcast(id) => {
-                format!("Podcast ID - {}", id)
-            }
-            Media::Placeholder => {
-                format!("Placeholder")
-            }
-        }
-    }
-
-    fn _description(&self) -> String {
-        if let Media::Book { title, author } = self {
-            format!("Book {} by {}", title, author)
-        } else if let Media::Movie { title, director } = self {
-            format!("Movie {} by {}", title, director)
-        } else if let Media::Audiobook { title } = self {
-            format!("Audiobook {}", title)
-        } else {
-            format!("Unknown media")
-        }
-    }
-}
-
-#[derive(Debug)]
-struct Catalog {
-    items: Vec<Media>,
-}
-
-impl Catalog {
-    fn new() -> Self {
-        Catalog { items: vec![] }
-    }
-
-    fn add(&mut self, item: Media) {
-        self.items.push(item);
-    }
-
-    fn get_by_index(&self, index: usize) -> Option<&Media> {
-        if index < self.items.len() {
-            Some(&self.items[index])
-        } else {
-            None
-        }
-    }
-}
-
-fn print_media(media: Media) {
-    println!("{:#?}", media)
-}
+use content::catalog::Catalog;
+use content::media::Media;
 
 fn main() {
     let book = Media::Book {
@@ -92,7 +27,7 @@ fn main() {
 
     // print_media(book);
     // print_media(movie);
-    // print_media(audiobook);
+    content::media::print_media(Media::Placeholder);
 
     let mut catalog = Catalog::new();
 
